7f4820d38debe3bc3d8fe369abbe4d4877c0da1c
lollckgacha <lollckgacha@gmail.com>
2026-01-16 01:34:53 +0900

    Add files via upload

---
+++ players.js
@@ -0,0 +1,213 @@
+const teamLogos = {
+    "BBQ": "images/logos/bbq.webp",
+    "BFX": "images/logos/bfx.svg",
+    "BRO": "images/logos/bro.svg",
+    "CJE": "images/logos/cje.svg",
+    "DK": "images/logos/dk.svg",
+    "DNS": "images/logos/dns.svg",
+    "DRX": "images/logos/drx.svg",
+    "EEW": "images/logos/eew.webp",
+    "GEN": "images/logos/gen.webp",
+    "GRF": "images/logos/grf.webp",
+    "HLE": "images/logos/hle.svg",
+    "JAG": "images/logos/jag.svg",
+    "KT": "images/logos/kt.svg",
+    "MVP": "images/logos/mvp.webp",
+    "NS": "images/logos/ns.svg",
+    "SBK": "images/logos/sbk.webp",
+    "SP": "images/logos/sp.webp",
+    "T1": "images/logos/t1.svg"
+};
+
+const playersByGrade = {
+    "legend": {
+        "top": [
+            { "name": "MaRin", "year": "LCK", "team": "LCK", "img": "images/players/0marin.webp" },
+            { "name": "Shy", "year": "LCK", "team": "LCK", "img": "images/players/0shy.jpg" },
+            { "name": "Smeb", "year": "LCK", "team": "LCK", "img": "images/players/0smeb.webp" }
+        ],
+        "jgl": [
+            { "name": "Ambition", "year": "LCK", "team": "LCK", "img": "images/players/0ambition.webp" },
+            { "name": "Bengi", "year": "LCK", "team": "LCK", "img": "images/players/0bengi.webp" },
+            { "name": "CloudTemplar", "year": "LCK", "team": "LCK", "img": "images/players/cloudtemplar.png" },
+            { "name": "Peanut", "year": "LCK", "team": "LCK", "img": "images/players/0peanut.png" },
+            { "name": "Score", "year": "LCK", "team": "LCK", "img": "images/players/0score.jpg" }
+        ],
+        "mid": [
+            { "name": "Kuro", "year": "LCK", "team": "LCK", "img": "images/players/0kuro.webp" },
+            { "name": "PawN", "year": "LCK", "team": "LCK", "img": "images/players/0pawn.webp" }
+        ],
+        "adc": [
+            { "name": "Bang", "year": "LCK", "team": "LCK", "img": "images/players/0bang.webp" },
+            { "name": "Deft", "year": "LCK", "team": "LCK", "img": "images/players/0deft.jpg" },
+            { "name": "PraY", "year": "LCK", "team": "LCK", "img": "images/players/0pray.webp" }
+        ],
+        "sup": [
+            { "name": "MadLife", "year": "LCK", "team": "LCK", "img": "images/players/0madlife.jpg" },
+            { "name": "Mata", "year": "LCK", "team": "LCK", "img": "images/players/0mata.jpg" },
+            { "name": "TusiN", "year": "LCK", "team": "LCK", "img": "images/players/18tusin.webp" },
+            { "name": "Wolf", "year": "LCK", "team": "LCK", "img": "images/players/0wolf.jpg" }
+        ]
+    },
+    "special": {
+        "top": [
+            { "name": "Nuguri", "year": "20", "team": "DK", "img": "images/players/20nuguri.webp" },
+            { "name": "Kingen", "year": "22", "team": "DRX", "img": "images/players/22kingen.webp" },
+            { "name": "Looper", "year": "14", "team": "GEN", "img": "images/players/14looper.webp" },
+            { "name": "CuVee", "year": "17", "team": "GEN", "img": "images/players/17cuvee.webp" },
+            { "name": "Kiin", "year": "24", "team": "GEN", "img": "images/players/24kiin.webp" },
+            { "name": "Kiin", "year": "25", "team": "GEN", "img": "images/players/kiin.webp" },
+            { "name": "Duke", "year": "18", "team": "LOL", "img": "images/players/18duke.webp" },
+            { "name": "TheShy", "year": "18", "team": "LOL", "img": "images/players/18theshy.webp" },
+            { "name": "GimGoon", "year": "19", "team": "LOL", "img": "images/players/19gimgoon.webp" },
+            { "name": "Impact", "year": "13", "team": "T1", "img": "images/players/13impact.webp" },
+            { "name": "MaRin", "year": "15", "team": "T1", "img": "images/players/15marin.webp" },
+            { "name": "Duke", "year": "16", "team": "T1", "img": "images/players/16duke.webp" },
+            { "name": "Huni", "year": "17", "team": "T1", "img": "images/players/17huni.webp" },
+            { "name": "Zeus", "year": "23", "team": "T1", "img": "images/players/23zeus.webp" },
+            { "name": "Zeus", "year": "24", "team": "T1", "img": "images/players/24zeus.webp" },
+            { "name": "Doran", "year": "25", "team": "T1", "img": "images/players/25doran.webp" }
+        ],
+        "jgl": [
+            { "name": "Canyon", "year": "20", "team": "DK", "img": "images/players/20canyon.webp" },
+            { "name": "Juhan", "year": "22", "team": "DRX", "img": "images/players/22juhan.webp" },
+            { "name": "Pyosik", "year": "22", "team": "DRX", "img": "images/players/22pyosik.webp" },
+            { "name": "Dandy", "year": "14", "team": "GEN", "img": "images/players/14dandy.webp" },
+            { "name": "Ambition", "year": "17", "team": "GEN", "img": "images/players/17ambition.webp" },
+            { "name": "Haru", "year": "17", "team": "GEN", "img": "images/players/17haru.webp" },
+            { "name": "Canyon", "year": "24", "team": "GEN", "img": "images/players/24canyon.webp" },
+            { "name": "Canyon", "year": "25", "team": "GEN", "img": "images/players/canyon.webp" },
+            { "name": "Kanavi", "year": "23", "team": "LOL", "img": "images/players/23kanavi.webp" },
+            { "name": "Bengi", "year": "13", "team": "T1", "img": "images/players/13bengi.webp" },
+            { "name": "Bengi", "year": "15", "team": "T1", "img": "images/players/15bengi.webp" },
+            { "name": "Bengi", "year": "16", "team": "T1", "img": "images/players/16bengi.webp" },
+            { "name": "Blank", "year": "16", "team": "T1", "img": "images/players/16blank.webp" },
+            { "name": "Blank", "year": "17", "team": "T1", "img": "images/players/17blank.webp" },
+            { "name": "Peanut", "year": "17", "team": "T1", "img": "images/players/17peanut.webp" },
+            { "name": "Oner", "year": "23", "team": "T1", "img": "images/players/23oner.webp" },
+            { "name": "Oner", "year": "24", "team": "T1", "img": "images/players/24oner.webp" },
+            { "name": "Oner", "year": "25", "team": "T1", "img": "images/players/25oner.webp" }
+        ],
+        "mid": [
+            { "name": "ShowMaker", "year": "20", "team": "DK", "img": "images/players/2020showmaker.webp" },
+            { "name": "Zeka", "year": "22", "team": "DRX", "img": "images/players/zeka.png" },
+            { "name": "PawN", "year": "14", "team": "GEN", "img": "images/players/14pawn.webp" },
+            { "name": "Crown", "year": "17", "team": "GEN", "img": "images/players/17crown.webp" },
+            { "name": "Chovy", "year": "24", "team": "GEN", "img": "images/players/24chovy.webp" },
+            { "name": "Chovy", "year": "25", "team": "GEN", "img": "images/players/chovy.webp" },
+            { "name": "Pawn", "year": "15", "team": "LOL", "img": "images/players/15pawn.webp" },
+            { "name": "Rooike", "year": "18", "team": "LOL", "img": "images/players/18rookie.webp" },
+            { "name": "DoinB", "year": "19", "team": "LOL", "img": "images/players/19doinb.webp" },
+            { "name": "Scout", "year": "21", "team": "LOL", "img": "images/players/21scout.webp" },
+            { "name": "Faker", "year": "13", "team": "T1", "img": "images/players/13faker.png" },
+            { "name": "Easyhoon", "year": "15", "team": "T1", "img": "images/players/15easyhoon.webp" },
+            { "name": "Faker", "year": "15", "team": "T1", "img": "images/players/15faker.webp" },
+            { "name": "Faker", "year": "16", "team": "T1", "img": "images/players/16faker.webp" },
+            { "name": "Faker", "year": "17", "team": "T1", "img": "images/players/17faker.webp" },
+            { "name": "Faker", "year": "23", "team": "T1", "img": "images/players/23faker.webp" },
+            { "name": "Faker", "year": "24", "team": "T1", "img": "images/players/24faker.webp" },
+            { "name": "Faker", "year": "25", "team": "T1", "img": "images/players/25faker.webp" }
+        ],
+        "adc": [
+            { "name": "Ghost", "year": "20", "team": "DK", "img": "images/players/20ghost.webp" },
+            { "name": "Deft", "year": "22", "team": "DRX", "img": "images/players/22deft.webp" },
+            { "name": "Imp", "year": "14", "team": "GEN", "img": "images/players/14imp.webp" },
+            { "name": "Ruler", "year": "17", "team": "GEN", "img": "images/players/17ruler.webp" },
+            { "name": "Peyz", "year": "24", "team": "GEN", "img": "images/players/24peyz.webp" },
+            { "name": "Ruler", "year": "25", "team": "GEN", "img": "images/players/ruler.webp" },
+            { "name": "Deft", "year": "15", "team": "LOL", "img": "images/players/15deft.webp" },
+            { "name": "Viper", "year": "21", "team": "LOL", "img": "images/players/21viper.webp" },
+            { "name": "Ruler", "year": "23", "team": "LOL", "img": "images/players/23ruler.webp" },
+            { "name": "Piglet", "year": "13", "team": "T1", "img": "images/players/13piglet.webp" },
+            { "name": "Bang", "year": "15", "team": "T1", "img": "images/players/15bang.webp" },
+            { "name": "Bang", "year": "16", "team": "T1", "img": "images/players/16bang.webp" },
+            { "name": "Bang", "year": "17", "team": "T1", "img": "images/players/17bang.webp" },
+            { "name": "Gumayusi", "year": "23", "team": "T1", "img": "images/players/23gumayusi.webp" },
+            { "name": "Gumayusi", "year": "24", "team": "T1", "img": "images/players/24gumayusi.webp" },
+            { "name": "Gumayusi", "year": "25", "team": "T1", "img": "images/players/25gumayusi.webp" }
+        ],
+        "sup": [
+            { "name": "BeryL", "year": "20", "team": "DK", "img": "images/players/20beryl.webp" },
+            { "name": "BeryL", "year": "22", "team": "DRX", "img": "images/players/22beryl.webp" },
+            { "name": "Mata", "year": "14", "team": "GEN", "img": "images/players/14mata.webp" },
+            { "name": "CoreJJ", "year": "17", "team": "GEN", "img": "images/players/17corejj.webp" },
+            { "name": "Lehends", "year": "24", "team": "GEN", "img": "images/players/24lehends.webp" },
+            { "name": "Duro", "year": "25", "team": "GEN", "img": "images/players/duro.webp" },
+            { "name": "PoohManDu", "year": "13", "team": "T1", "img": "images/players/13poohmandu.webp" },
+            { "name": "Wolf", "year": "15", "team": "T1", "img": "images/players/15wolf.webp" },
+            { "name": "Wolf", "year": "16", "team": "T1", "img": "images/players/16wolf.webp" },
+            { "name": "Wolf", "year": "17", "team": "T1", "img": "images/players/17wolf.webp" },
+            { "name": "Keria", "year": "23", "team": "T1", "img": "images/players/23keria.webp" },
+            { "name": "Keria", "year": "24", "team": "T1", "img": "images/players/24keria.webp" },
+            { "name": "Keria", "year": "25", "team": "T1", "img": "images/players/25keria.webp" }
+        ]
+    },
+    "normal": {
+        "top": [
+            { "name": "Clear", "year": "26", "team": "BFX", "img": "images/players/clear.webp" },
+            { "name": "Casting", "year": "26", "team": "BRO", "img": "images/players/casting.webp" },
+            { "name": "Siwoo", "year": "26", "team": "DK", "img": "images/players/siwoo.webp" },
+            { "name": "DuDu", "year": "26", "team": "DNS", "img": "images/players/dudu.webp" },
+            { "name": "Rich", "year": "26", "team": "DRX", "img": "images/players/rich.webp" },
+            { "name": "Kiin", "year": "26", "team": "GEN", "img": "images/players/kiin.webp" },
+            { "name": "Zeus", "year": "26", "team": "HLE", "img": "images/players/zeus.png" },
+            { "name": "PerfecT", "year": "26", "team": "KT", "img": "images/players/perfect.webp" },
+            { "name": "Kingen", "year": "26", "team": "NS", "img": "images/players/kingen.webp" },
+            { "name": "Doran", "year": "26", "team": "T1", "img": "images/players/doran.png" }
+        ],
+        "jgl": [
+            { "name": "Raptor", "year": "26", "team": "BFX", "img": "images/players/raptor.webp" },
+            { "name": "GIDEON", "year": "26", "team": "BRO", "img": "images/players/gideon.webp" },
+            { "name": "Lucid", "year": "26", "team": "DK", "img": "images/players/lucid.webp" },
+            { "name": "Pyosik", "year": "26", "team": "DNS", "img": "images/players/pyosik.webp" },
+            { "name": "Vincenzo", "year": "26", "team": "DRX", "img": "images/players/vincenzo.webp" },
+            { "name": "Willer", "year": "26", "team": "DRX", "img": "images/players/willer.webp" },
+            { "name": "Canyon", "year": "26", "team": "GEN", "img": "images/players/canyon.webp" },
+            { "name": "Kanavi", "year": "26", "team": "HLE", "img": "images/players/kanavi.png" },
+            { "name": "Cuzz", "year": "26", "team": "KT", "img": "images/players/cuzz.webp" },
+            { "name": "Sponge", "year": "26", "team": "NS", "img": "images/players/sponge.webp" },
+            { "name": "Oner", "year": "26", "team": "T1", "img": "images/players/oner.png" }
+        ],
+        "mid": [
+            { "name": "Daystar", "year": "26", "team": "BFX", "img": "images/players/daystar.webp" },
+            { "name": "VicLa", "year": "26", "team": "BFX", "img": "images/players/vicla.webp" },
+            { "name": "Fisher", "year": "26", "team": "BRO", "img": "images/players/fisher.webp" },
+            { "name": "Roamer", "year": "26", "team": "BRO", "img": "images/players/roamer.webp" },
+            { "name": "ShowMaker", "year": "26", "team": "DK", "img": "images/players/showmaker.webp" },
+            { "name": "Clozer", "year": "26", "team": "DNS", "img": "images/players/clozer.webp" },
+            { "name": "Ucal", "year": "26", "team": "DRX", "img": "images/players/ucal.webp" },
+            { "name": "Chovy", "year": "26", "team": "GEN", "img": "images/players/chovy.webp" },
+            { "name": "Zeka", "year": "26", "team": "HLE", "img": "images/players/zeka.webp" },
+            { "name": "Bdd", "year": "26", "team": "KT", "img": "images/players/bdd.webp" },
+            { "name": "Calix", "year": "26", "team": "NS", "img": "images/players/calix.webp" },
+            { "name": "Scout", "year": "26", "team": "NS", "img": "images/players/scout.webp" },
+            { "name": "Faker", "year": "26", "team": "T1", "img": "images/players/faker.png" }
+        ],
+        "adc": [
+            { "name": "Diable", "year": "26", "team": "BFX", "img": "images/players/diable.webp" },
+            { "name": "Teddy", "year": "26", "team": "BRO", "img": "images/players/teddy.webp" },
+            { "name": "Smash", "year": "26", "team": "DK", "img": "images/players/smash.webp" },
+            { "name": "deokdam", "year": "26", "team": "DNS", "img": "images/players/deokdam.webp" },
+            { "name": "Jiwoo", "year": "26", "team": "DRX", "img": "images/players/jiwoo.webp" },
+            { "name": "Ruler", "year": "26", "team": "GEN", "img": "images/players/ruler.webp" },
+            { "name": "Gumayusi", "year": "26", "team": "HLE", "img": "images/players/gumayusi.png" },
+            { "name": "Aiming", "year": "26", "team": "KT", "img": "images/players/aiming.webp" },
+            { "name": "Taeyoon", "year": "26", "team": "NS", "img": "images/players/taeyoon.webp" },
+            { "name": "Peyz", "year": "26", "team": "T1", "img": "images/players/peyz.png" }
+        ],
+        "sup": [
+            { "name": "Kellin", "year": "26", "team": "BFX", "img": "images/players/kellin.webp" },
+            { "name": "Namgung", "year": "26", "team": "BRO", "img": "images/players/namgung.webp" },
+            { "name": "Career", "year": "26", "team": "DK", "img": "images/players/career.webp" },
+            { "name": "Life", "year": "26", "team": "DNS", "img": "images/players/life.webp" },
+            { "name": "Peter", "year": "26", "team": "DNS", "img": "images/players/peter.webp" },
+            { "name": "Andil", "year": "26", "team": "DRX", "img": "images/players/andil.webp" },
+            { "name": "Duro", "year": "26", "team": "GEN", "img": "images/players/duro.webp" },
+            { "name": "Delight", "year": "26", "team": "HLE", "img": "images/players/delight.png" },
+            { "name": "Ghost", "year": "26", "team": "KT", "img": "images/players/ghost.webp" },
+            { "name": "Pollu", "year": "26", "team": "KT", "img": "images/players/pollu.webp" },
+            { "name": "Lehends", "year": "26", "team": "NS", "img": "images/players/lehends.webp" },
+            { "name": "Keria", "year": "26", "team": "T1", "img": "images/players/keria.png" }
+        ]
+    }
+};
